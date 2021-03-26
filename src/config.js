5bea5acea7ee47d3711a9b52293687a554739564
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-03-26 02:13:26 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   isProd: process.env.NODE_ENV === 'production',
   hmrEnabled: process.env.NODE_ENV !== 'production' && !process.env.NO_HMR,
   distFolder: path.resolve(__dirname, '../dist'),
-  publicPath: '/assets',
+  publicPath: '/assets/',
   wdsPort: 3001,
 };
