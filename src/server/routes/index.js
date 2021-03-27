ecfb7c934994c09105016248c4d7bce2cdeb2a09
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-03-27 00:47:51 -0500

    search route. check if string is exist and check if username is clean.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,8 +6,28 @@ const router = express.Router();
 
 module.exports = () => {
   router.get('/', (req, res) => {
-    // console.log(req);
-    res.render('index', {});
+    const { search } = req.query;
+    console.log('Search', search);
+    if (typeof search === 'string') {
+      if (search && search.length >= 4 && search.length <= 25) {
+        if (isAlphanumeric(search)) {
+          // need to check username. is username exist from twitch?
+          // if yes, show result. if no. inform them, username is not exist
+          return res.render('index', {
+            username: search,
+          });
+        }
+        return res.render('index', {
+          username: search,
+          warningMessage: '* Username must only contain alphanumeric characters.',
+        });
+      }
+      return res.render('index', {
+        username: search,
+        warningMessage: '* Username must be between 4 and 25 characters.',
+      });
+    }
+    res.render('index');
   });
   return router;
 };
