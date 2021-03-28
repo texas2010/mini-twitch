f7a9cdb678b2e36a22b3553f483257049c60aae2
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-03-27 20:57:36 -0500

    access isUsernameExist to check username is exist or not in the route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,19 +2,35 @@ const express = require('express');
 
 const { isAlphanumeric } = require('../../../utils');
 
+const Twitch = require('../twitch');
+
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
+    console.log(Twitch);
     const { search } = req.query;
-    console.log('Search', search);
     if (typeof search === 'string') {
       if (search && search.length >= 4 && search.length <= 25) {
         if (isAlphanumeric(search)) {
           // need to check username. is username exist from twitch?
           // if yes, show result. if no. inform them, username is not exist
+          const isUsernameExist = await Twitch.isUsernameExist(search);
+          if (isUsernameExist && isUsernameExist.error) {
+            return res.render('index', {
+              username: search,
+              warningMessage: isUsernameExist.error,
+            });
+          }
+          if (isUsernameExist.isUsernameExist) {
+            return res.render('index', {
+              username: search,
+              usernameExistMessage: 'Success! We will show result!',
+            });
+          }
           return res.render('index', {
             username: search,
+            warningMessage: '* Username is not exist in the Twitch.',
           });
         }
         return res.render('index', {
