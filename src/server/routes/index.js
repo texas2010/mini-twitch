28769a147a39f45a0e3aff9292f32d3320132010
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-04-03 19:12:39 -0500

    fix some code in index route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,36 +1,32 @@
 const express = require('express');
 
-const { isAlphanumeric } = require('../../../utils');
-
 const Twitch = require('../twitch');
+const { isAlphanumeric } = require('../../../utils');
 
 const router = express.Router();
 
 module.exports = () => {
   router.get('/', async (req, res) => {
-    console.log(Twitch);
     const { search } = req.query;
     if (typeof search === 'string') {
       if (search && search.length >= 4 && search.length <= 25) {
         if (isAlphanumeric(search)) {
-          // need to check username. is username exist from twitch?
-          // if yes, show result. if no. inform them, username is not exist
           const isUsernameExist = await Twitch.isUsernameExist(search);
-          if (isUsernameExist && isUsernameExist.error) {
+          if (isUsernameExist.hasOwnProperty('exist')) {
+            if (isUsernameExist.exist) {
+              return res.render('index', {
+                username: search,
+                usernameExistMessage: 'Success! We will show result!',
+              });
+            }
             return res.render('index', {
               username: search,
-              warningMessage: isUsernameExist.error,
-            });
-          }
-          if (isUsernameExist.isUsernameExist) {
-            return res.render('index', {
-              username: search,
-              usernameExistMessage: 'Success! We will show result!',
+              warningMessage: '* Username is not exist in the Twitch.',
             });
           }
           return res.render('index', {
             username: search,
-            warningMessage: '* Username is not exist in the Twitch.',
+            warningMessage: isUsernameExist.error,
           });
         }
         return res.render('index', {
