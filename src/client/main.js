c61e3a629749800be900169e8e68c9768f3ff225
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-05-12 12:54:13 -0500

    feature of filter online or offline stream

--- src/client/main.js
+++ src/client/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import './main.scss'; // Do not remove this line
 
 const { isAlphanumeric } = require('../../utils/index');
@@ -5,6 +6,8 @@ const { isAlphanumeric } = require('../../utils/index');
 const searchInput = document.querySelector('#search input');
 const searchButton = document.querySelector('#search button');
 const searchWarningMessage = document.querySelector('#search-warning');
+const filterButtons = document.querySelector('#filter-buttons');
+const showStream = document.querySelector('#show-stream');
 
 searchInput.addEventListener('input', (e) => {
   const { value: inputValue } = e.target;
@@ -25,5 +28,48 @@ searchInput.addEventListener('input', (e) => {
     searchButton.disabled = true;
     searchWarningMessage.textContent = '* Username must be between 4 and 25 characters.';
     searchWarningMessage.classList.add('p-1');
+  }
+});
+
+filterButtons.addEventListener('click', (e) => {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  switch (e.target.name) {
+    case 'all':
+      if (showStream.classList.contains('online')) {
+        showStream.classList.remove('online');
+        filterButtons
+          .querySelector(`button[name='online']`)
+          .classList.replace('btn-success', 'btn-primary');
+      } else if (showStream.classList.contains('offline')) {
+        showStream.classList.remove('offline');
+        filterButtons
+          .querySelector(`button[name='offline']`)
+          .classList.replace('btn-success', 'btn-primary');
+      }
+      break;
+    case 'online':
+      if (showStream.classList.contains('offline')) {
+        showStream.classList.remove('offline');
+        filterButtons
+          .querySelector(`button[name='offline']`)
+          .classList.replace('btn-success', 'btn-primary');
+      }
+      showStream.classList.add('online');
+      e.target.classList.replace('btn-primary', 'btn-success');
+      break;
+    case 'offline':
+      if (showStream.classList.contains('online')) {
+        showStream.classList.remove('online');
+        filterButtons
+          .querySelector(`button[name='online']`)
+          .classList.replace('btn-success', 'btn-primary');
+      }
+      showStream.classList.add('offline');
+      e.target.classList.replace('btn-primary', 'btn-success');
+      break;
+    default:
+      break;
   }
 });
